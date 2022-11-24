167590df1958cf7913d9b2fa83d93667aba78e2e
aenz <andrewjensor@gmail.com>
2022-11-23 17:25:54 -0800

    creating addEmployee + finishing index.js

--- index.js
+++ index.js
@@ -79,3 +79,133 @@ const addManager = () => {
     })
 };
 
+const addEmployee = () => {
+    return inquirer.prompt ([
+        {
+            type: 'list',
+            name: 'role',
+            message: 'Choose a role for this Employee',
+            choices: ['Engineer', 'Intern']
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Employee Name', 
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log ('Invalid employee name');
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Please enter the employee ID',
+            validate: nameInput => {
+                if  (isNaN(nameInput)) {
+                    console.log ('Please enter a valid employee ID')
+                    return false; 
+                } else {
+                    return true;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: `Enter manager's email`,
+            validate: email => {
+                valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+                if (valid) {
+                    return true
+                } else {
+                    console.log ("Invalid email");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: `Please enter the employee's github username.`,
+            when: (input) => input.role === "Engineer",
+            validate: nameInput => {
+                if (nameInput ) {
+                    return true;
+                } else {
+                    console.log ("Please enter a valid employee github username")
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: `Please enter the intern's school`,
+            when: (input) => input.role === "Intern",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log ('Please enter a valid school for this intern')
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddingEmployee',
+            message: 'Would you like to add more team members?',
+            default: false
+        }
+    ])
+    .then(employeeData => {
+        // data for employee types 
+
+        let { name, id, email, role, github, school, confirmAddEmployee } = employeeData; 
+        let employee; 
+
+        if (role === "Engineer") {
+            employee = new Engineer (name, id, email, github);
+
+            console.log(employee);
+
+        } else if (role === "Intern") {
+            employee = new Intern (name, id, email, school);
+
+            console.log(employee);
+        }
+
+        teamArray.push(employee); 
+
+        if (confirmAddEmployee) {
+            return addEmployee(teamArray); 
+        } else {
+            return teamArray;
+        }
+    })
+};
+
+const createFile = data => {
+    fs.createFile('./dist/index.html', data, err => {
+        if (err) {
+            console.log(err)
+            return;
+        } else {
+            console.log('Successfully created a team profile!')
+        }
+    })
+};
+
+addManager()
+    .then(addEmployee)
+    .then(teamArray => {
+        return generateHTML(teamArray);
+    })
+    .then(pageHTML => {
+        return createFile(pageHTML);
+    })
+    .catch(err => {
+        console.log(err);
+    });
